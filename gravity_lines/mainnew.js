81a5fd4a777ab11f8e92156863cba8431f0f660b
Sam Kilgus <samkilgus@gmail.com>
2018-06-21 16:07:40 -0400

    fixing gravity lines

--- gravity_lines/mainnew.js
+++ gravity_lines/mainnew.js
@@ -47,15 +47,30 @@ function getVector(x,y,r,d){
     return [x+Math.cos(r)*d,y+Math.sin(r)*d];
 }
 
+function indToCoord(i) {
+    i = i / 4;
+    var y = parseInt(i / width, 10);
+    var x = i - y * width;
+    return [x,y];
+}
+
+function coordToInd(x,y) {
+    return (x + (y * width))*4;
+}
+
 /******************************************************************************/
 /************************** WET ***********************************************/
 /******************************************************************************/
+const CONNECT = false;
 
-const CANV_RATIO = 0.8;
-const GRAV_DIST_MULT = 2.5;
+const CANV_RATIO = 2;
+const GRAV_DIST_MULT = 3.5;
 
 var canvas;
 var context;
+
+var tempCanvas;
+var tempContext;
 
 const colorPallete = ["#000000", "#050505", "#101010", "#151515", "#202020", "#252525"];
 
@@ -67,13 +82,13 @@ var width,
     wetEl,
     wetInterval;
 
-var buffer = [];
+var temp_circles;
 
 var slow = false;
 
 window.onresize = function() {
-    width = canvas.width = window.innerWidth * CANV_RATIO;
-    height = canvas.height = window.innerHeight * CANV_RATIO;
+    width = canvas.width = tempCanvas.width = window.innerWidth * CANV_RATIO;
+    height = canvas.height = tempCanvas.height = window.innerHeight * CANV_RATIO;
     wetSrc.x = width / 2;
     wetSrc.y = height / 2;
 }
@@ -89,7 +104,10 @@ class Circle {
         // this.vx = 0;
         // this.vy = 0;
 
-        this.r  = (setr ? setr : ((10 + (50 * Math.random())) * CANV_RATIO));
+        this.r  = 200//(setr ? setr : ((50 + (100 * Math.random())) * CANV_RATIO));
+
+        this.fc = "white";
+        this.sc = false;
     }
 
     equals(c) {
@@ -104,25 +122,49 @@ class Circle {
         this.y += this.vy/5;
         var rad2ed = Math.pow(this.r,2);
 
-        // sort circles by distance to current circle
-
-        var temp_circles = circles.slice()
-            .sort((a,b) => (getDist(this.x,this.y,a.x,a.y) -
-                            getDist(this.x,this.y,b.x,b.y)));
         // apply gravity
-        for (var i = 0; i < temp_circles.length; i++) {
-            var b = temp_circles[i];
-            if (this.equals(b)) continue;
-            var d = getDist(this.x,this.y,b.x,b.y);
-            const maxDist = this.r + b.r * GRAV_DIST_MULT;
-            var brad2 = Math.pow(b.r,2);
-            if (d < maxDist) {
-                var mult = Math.pow((maxDist-d)/maxDist,2) * 0.01;
-                this.vx += ((b.x - this.x) * mult * (brad2 / rad2ed));
-                this.vy += ((b.y - this.y) * mult * (brad2 / rad2ed));
-            }
-            else return;
+        // for (var i = 0; i < temp_circles.length; i++) {
+        //     var b = temp_circles[i];
+        //     if (this.equals(b)) continue;
+        //     var d = getDist(this.x,this.y,b.x,b.y);
+        //     const maxDist = this.r + b.r * GRAV_DIST_MULT;
+        //     var brad2 = Math.pow(b.r,2);
+        //     if (d < maxDist) {
+        //         var mult = Math.pow((maxDist-d)/maxDist,2) * 0.01;
+        //         this.vx += ((b.x - this.x) * mult * (brad2 / rad2ed));
+        //         this.vy += ((b.y - this.y) * mult * (brad2 / rad2ed));
+        //     }
+        //     else return;
+        // }
+    }
+
+    render() {
+        tempContext.beginPath();
+
+        tempContext.arc(this.x, this.y, Math.max(0,this.r), 0, Math.PI * 2, true);
+
+        if (this.fc) {
+            tempContext.fillStyle = this.fc;
+            tempContext.fill();
         }
+        if (this.sc) {
+            tempContext.strokeStyle = this.sc;
+            tempContext.stroke();
+        }
+
+        tempContext.closePath();
+    }
+
+    connect(b,d,m) {
+        if (this.r === 0 || b.r === 0) return;
+
+        // Generate the connector path
+        tempContext.beginPath();
+
+        tempContext.moveTo(this.x,this.y);
+        tempContext.lineTo(b.x,b.y);
+        tempContext.stroke();
+        tempContext.closePath();
     }
 }
 
@@ -138,62 +180,70 @@ function newCurve(sx,sy,c1x,c1y,c2x,c2y,fx,fy) {
     return [sx,sy,c1x,c1y,c2x,c2y,fx,fy]
 }
 
-function drawConnection(x1,y1,rad1, x2,y2,rad2) {
-    const d = getDist(x1,y1,x2,y2);
-    const maxDist = rad1 + rad2 * GRAV_DIST_MULT;
-    if (rad1 === 0 || rad2 === 0 || d > maxDist) return;
-
-    // Generate the connector path
-    context.beginPath();
-
-    context.moveTo(x1,y1);
-    context.lineTo(x2,y2);
-
-
-    context.stroke();
-    context.closePath();
-}
-
 var curRender = 0;
 
 function renderCircles() {
     curRender++;
 
-    context.fillStyle = "rgba(0,0,0,1)";
-    // context.fillStyle = "white";
-    context.fillRect(0, 0, width, height);
+    context.clearRect(0, 0, width, height);
+    tempContext.clearRect(0, 0, width, height);
+
+    var connected = {};
 
     for (var i = 0; i < circles.length; i++) {
         var a = circles[i];
 
-        context.beginPath();
+        a.render();
 
-        context.arc(a.x, a.y, Math.max(0,a.r), 0, Math.PI * 2, true);
+        temp_circles = circles.slice()
+            .sort((b,c) => (getDist(a.x,a.y,b.x,b.y) -
+                            getDist(a.x,a.y,c.x,c.y)));
 
-        context.fillStyle = "black";
-        context.fill();
-        context.stroke();
-        context.closePath();
-
-        context.fillStyle = context.strokeStyle = "white";
-        for (var j = i+1; j < circles.length; j++) {
-            var b = circles[j];
-            drawConnection(a.x,a.y,a.r, b.x,b.y,b.r);
+        for (var j = 0; j < temp_circles.length && CONNECT; j++) {
+            if (j == i || (connected[i] && connected[i].indexOf(j) >= 0)) continue;
+            var b = temp_circles[j];
+            var d = getDist(a.x,a.y,b.x,b.y);
+            var maxDist = a.r + b.r * GRAV_DIST_MULT;
+            if (d > maxDist) break;
+            a.connect(b,d,maxDist);
+            if (!connected[i]) connected[i] = [];
+            connected[i].push(j);
         }
 
         a.update();
     }
 
+    // draw circle from click
     if (tempCirc) {
-        context.beginPath();
+        tempContext.beginPath();
 
-        context.arc(tempCirc.x, tempCirc.y, Math.max(0,tempCirc.r), 0, Math.PI * 2, true);
+        tempContext.arc(tempCirc.x, tempCirc.y, Math.max(0,tempCirc.r), 0, Math.PI * 2, true);
 
-        context.fillStyle = "white";
-        context.fill();
-        // context.stroke();
-        context.closePath();
+        tempContext.fillStyle = "white";
+        tempContext.fill();
+        tempContext.closePath();
     }
+
+    var imgData = tempContext.createImageData(width,height);
+    var tempImgData = tempContext.getImageData(0,0,width,height);
+    var adsfasdfadsf = 0;
+    for (var i = 0; i < imgData.data.length; i+=4) {
+        if (tempImgData.data[i+3] < 255) continue;
+        adsfasdfadsf++;
+        var coord = indToCoord(i);
+        var x = Math.round(coord[0] + (dispMap[coord[1]][coord[0]][0] * 30));
+        var y = Math.round(coord[1] + (dispMap[coord[1]][coord[0]][1] * 30));
+
+        var newI = coordToInd(x,y);
+        if (i == newI) continue;
+        imgData.data[newI]   = tempImgData.data[i]
+        imgData.data[newI+1] = tempImgData.data[i+1]
+        imgData.data[newI+2] = tempImgData.data[i+2]
+        imgData.data[newI+3] = tempImgData.data[i+3]
+    }
+    // console.log("num: " + adsfasdfadsf);
+
+    context.putImageData(imgData,0,0);
 
     removeCircles();
     if (slow) {
@@ -273,10 +323,34 @@ function initCanvas() {
     context = canvas.getContext("2d");
     width = canvas.width = window.innerWidth * CANV_RATIO;
     height = canvas.height = window.innerHeight * CANV_RATIO;
-    context.fillStyle = "white";
-    context.fillRect(0,0,width,height);
-    context.fillStyle = "rgba(0,0,0,0.07)";
-    for (var i = 0; i < 100; i++) context.fillRect(0,0,width,height);
+
+
+    tempCanvas  = document.createElement("canvas");
+    tempContext = tempCanvas.getContext("2d");
+    tempCanvas.width   = width;
+    tempCanvas.height = height;
+
+    generateDisplacementMap();
+}
+
+/******************************************************************************/
+/************************** DISPLACEBMENT MAP *********************************/
+/******************************************************************************/
+
+var dispMap;
+
+function generateDisplacementMap() {
+    dispMap = [];
+    var mod1 = chance.integer({min:10,max:50});
+    var mod2 = chance.integer({min:10,max:50});
+    for (var y = 0; y < height; y++) {
+        dispMap.push([]);
+        for (var x = 0; x < width; x++) {
+            dispMap[y].push([]);
+            dispMap[y][x] = [noise.perlin2(x*mod1/width,y*mod1/height),noise.perlin2(x*mod2/width,y*mod2/height)];
+        }
+    }
+    console.log(dispMap);
 }
 
 /******************************************************************************/
